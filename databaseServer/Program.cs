d092c2c5511acf3a893afddfd547cc0feb5fe1bf
Henrique6006 <HenriqueB321@hotmail.com>
2023-11-18 15:39:10 -0300

    Classe Operacao

---
+++ databaseServer/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace databaseServer{
+    class Program{
+
+        static void Main(string[] args){
+            //Classe Main do Servidor
+        }
+    }
+}
